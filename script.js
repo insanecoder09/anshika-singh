3fe9cde46ce527c2a17ce3afc10fc2d2eb8b64f3
insanecoder09 <147096842+insanecoder09@users.noreply.github.com>
2023-11-09 02:06:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var close = document.querySelector('.ri-close-line');
+var menu = document.querySelector('.ri-menu-3-line');
+var main = document.querySelector('.main');
+var time = document.querySelector('.zoom');
+
+let i = 1
+setInterval(function(){
+    time.innerHTML = i;
+    i = i+1;
+},15)
+
+function firstPageAnimation(){
+    var tl = gsap.timeline();
+
+    setTimeout(function(){
+        tl.to('.boundingelem',{
+            translateY: 0,
+            ease: Expo.easeInOut,
+            duration: 3,
+            delay: -1,
+            stagger: .5
+        })
+
+        tl.from('.pic',{
+            opacity: 0,
+            translateY: -20,
+            ease: Expo.easeInOut,
+            duration: 4,
+            delay: -3,
+            stagger: .5
+        })
+    },1500)
+} firstPageAnimation();
+
+menu.addEventListener('click', function(){
+    gsap.to('.menuScreen',{
+        top:0,
+        ease: Expo.easeInOut,
+        duration: 3,
+        delay: -1,
+    })
+})
+
+close.addEventListener('click', function(){
+    gsap.to('.menuScreen',{
+        top:-120 + "%"
+    })
+})
+
+document.querySelector('.loading').style.height = window.innerHeight + "px"
+
+function loading(){
+    setTimeout(function(){
+        gsap.to('.loading',{
+            top: -100 + "%"
+        })
+    },1500)
+}
+loading();
